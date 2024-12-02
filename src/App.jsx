41178c6e63ec051a004ecddbef6d91bdade07daa
CodeWithAryansh07 <codewitharyansh07@gmail.com>
2024-12-02 21:02:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <h1 className="">sfadas</h1> */}
+      <Header />
+      <Hero />
+      <Footer />
+    </>
+  )
+}
+
+export default App
